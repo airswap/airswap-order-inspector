e3d2593a2d0d305bc0391e50edc424e43f914e35
Mike Starr <imhs@protonmail.com>
2023-11-09 13:00:30 -0800

    setup order object

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
 import React, { useState } from 'react';
 import './App.css';
+import { useContractRead } from 'wagmi';
+import swapAbi from '../swapAbi.json';
+import { zeroAddress } from 'viem';
+
+interface checkParamsJSON {
+  nonce: string;
+  expiry: string;
+  signerWallet: string;
+  signerToken: string;
+  signerAmount: string;
+  senderToken: string;
+  senderAmount: string;
+  v: string;
+  r: string;
+  s: string;
+}
 
 function App() {
-  const [json, setJson] = useState<undefined | string>();
+  const [jsonString, setJsonString] = useState<undefined | string>();
+  const [parsedJSON, setParsedJSON] = useState<undefined | checkParamsJSON>();
+
+  const swapContractAddress = '0xb926D88D6BdD560383fCd6537bbf5Aa863470318';
+
+  const order = {
+    nonce: parsedJSON?.nonce || '',
+    expiry: parsedJSON?.expiry || '',
+    signerWallet: parsedJSON?.signerWallet || '',
+    signerToken: parsedJSON?.signerToken || '',
+    signerAmount: parsedJSON?.signerAmount || '',
+    senderToken: parsedJSON?.senderToken || '',
+    senderAmount: parsedJSON?.senderAmount || '',
+    v: parsedJSON?.v || '',
+    r: parsedJSON?.r || '',
+    s: parsedJSON?.s || '',
+  };
+
+  const { data, isError } = useContractRead({
+    address: swapContractAddress,
+    abi: swapAbi,
+    functionName: 'check',
+    args: [parsedJSON?.signerWallet || zeroAddress, order],
+    enabled: !!jsonString,
+  });
+
+  console.log(data, isError);
+
+  React.useEffect(() => {
+    if (jsonString) {
+      const parsedJson = JSON.parse(jsonString);
+      setParsedJSON(parsedJson);
+    }
+  }, [jsonString]);
+
   return (
     <>
       <h1>AirSwap Debugger:</h1>
@@ -20,9 +70,9 @@ function App() {
           name="json"
           placeholder="paste your JSON here..."
           autoComplete="off"
-          onChange={(e) => setJson(e.target.value)}
+          onChange={(e) => setJsonString(e.target.value)}
         />
-        <button>debug</button>
+        <button>Check for errors</button>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={config}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>
+);
