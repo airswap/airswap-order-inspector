a68f401036e18b5e75dce21b209022cca5c6af66
Mike Starr <imhs@protonmail.com>
2023-11-12 14:12:10 -0800

    updated validateJsonShape function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, MouseEvent, useState } from 'react';
+import { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
 import './App.css';
 import { useContractRead } from 'wagmi';
 import swapERC20ABI from '../swapERC20ABI.json';
@@ -12,61 +12,79 @@ function App() {
   const [parsedJSON, setParsedJSON] = useState<
     undefined | Partial<checkParamsJSON>
   >(undefined);
-  const [errors, setErrors] = useState<boolean | string>(false);
+  const [errors, setErrors] = useState<boolean | string | Error>(false);
   const [isError, setIsError] = useState(false);
+  const [isEnableCheck, setEnableCheck] = useState(false);
 
-  // const { error } = useContractRead({
-  //   address: swapContractAddress,
-  //   abi: swapERC20ABI,
-  //   functionName: 'check',
-  //   args: [
-  //     parsedJSON?.senderWallet || zeroAddress,
-  //     Number(parsedJSON?.nonce) || 0,
-  //     Number(parsedJSON?.expiry) || 0,
-  //     parsedJSON?.signerWallet || zeroAddress,
-  //     parsedJSON?.signerToken || zeroAddress,
-  //     (parsedJSON?.signerAmount && BigInt(parsedJSON?.signerAmount)) ||
-  //       BigInt(0),
-  //     parsedJSON?.senderToken || zeroAddress,
-  //     (parsedJSON?.senderAmount && BigInt(parsedJSON?.senderAmount)) ||
-  //       BigInt(0),
-  //     Number(parsedJSON?.v) || 0,
-  //     parsedJSON?.r || '0x',
-  //     parsedJSON?.s || '0x',
-  //   ],
-  //   // enabled: false,
-  //   enabled: isCheckErrors,
-  // });
+  const { error: checkFunctionError } = useContractRead({
+    address: swapContractAddress,
+    abi: swapERC20ABI,
+    functionName: 'check',
+    args: [
+      parsedJSON?.senderWallet || zeroAddress,
+      Number(parsedJSON?.nonce) || 0,
+      Number(parsedJSON?.expiry) || 0,
+      parsedJSON?.signerWallet || zeroAddress,
+      parsedJSON?.signerToken || zeroAddress,
+      (parsedJSON?.signerAmount && BigInt(parsedJSON?.signerAmount)) ||
+        BigInt(0),
+      parsedJSON?.senderToken || zeroAddress,
+      (parsedJSON?.senderAmount && BigInt(parsedJSON?.senderAmount)) ||
+        BigInt(0),
+      Number(parsedJSON?.v) || 0,
+      parsedJSON?.r || '0x',
+      parsedJSON?.s || '0x',
+    ],
+    enabled: isEnableCheck,
+  });
+
+  const handleChangeTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setEnableCheck(false);
+    setJsonString(e.target.value);
+  };
 
   const handleSubmit = (e: MouseEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log('click');
 
     try {
       const parsedJsonString = jsonString && JSON.parse(jsonString);
       setParsedJSON(parsedJsonString);
     } catch (e) {
-      console.error(e);
-      console.log('console.log e:', e);
       setErrors(`Your input is not valid JSON format:\n\n${e}`);
       setIsError(true);
       return;
     }
+  };
 
+  useEffect(() => {
+    // This block will be executed whenever parsedJSON is updated
     if (parsedJSON) {
       const isValidJsonShape = validateJsonShape(parsedJSON);
-      if (!isValidJsonShape) {
+
+      if (isValidJsonShape) {
         setErrors(isValidJsonShape);
         setIsError(true);
+        return;
       } else {
-        setErrors('Looks good! No errors! 🎊');
-        setIsError(false);
+        // run check function on smart contract
+        setEnableCheck(true);
       }
     }
-  };
+  }, [parsedJSON]);
 
-  const handleChangeTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setJsonString(e.target.value);
-  };
+  useEffect(() => {
+    // This block will be executed whenever checkFunctionError is updated
+
+    if (checkFunctionError && isEnableCheck) {
+      setIsError(true);
+      console.log('checkFunctionError', checkFunctionError);
+      setErrors(checkFunctionError.message);
+    } else if (!checkFunctionError && isEnableCheck) {
+      setIsError(false);
+      setErrors('Your JSON input Looks good with no errors! 🎊');
+    }
+  }, [checkFunctionError, isEnableCheck]);
 
   return (
     <>
@@ -93,7 +111,7 @@ function App() {
               border: !isError ? 'none' : 'solid gray 1px',
             }}
           >
-            {errors}
+            {typeof errors === 'string' ? errors : null}
           </div>
         )}
       </div>
--- src/helpers/validateJsonShape.ts
+++ src/helpers/validateJsonShape.ts
@@ -1,3 +1,4 @@
+import { isAddress } from 'viem';
 import { checkParamsJSON } from '../../types';
 
 export const validateJsonShape = (
@@ -18,14 +19,40 @@ export const validateJsonShape = (
       's',
     ];
 
+    // Check that the type of each value is valid
+    if (json) {
+      if (json['senderWallet'] && !isAddress(json['senderWallet'])) {
+        return 'senderWallet must be a valid ERC20 address';
+      } else if (json['nonce'] && isNaN(json['nonce'])) {
+        return 'nonce must be a number. Make sure it\'s wrapped in quotation marks, e.g. "99"';
+      } else if (json['expiry'] && isNaN(json['expiry'])) {
+        return 'expiry must be a number. Make sure it\'s wrapped in quotation marks, e.g. "1566941284"';
+      } else if (json['signerWallet'] && !isAddress(json['signerWallet'])) {
+        return 'signerWallet must be a valid ERC20 address';
+      } else if (json['signerToken'] && !isAddress(json['signerToken'])) {
+        return 'signerToken must be a valid ERC20 address';
+      } else if (json['signerAmount'] && !isNaN(Number(json['signerAmount']))) {
+        return 'signerAmount must be a valid ERC20 address';
+      } else if (json['senderToken'] && !isAddress(json['senderToken'])) {
+        return 'senderToken must be a valid ERC20 address';
+      } else if (json['senderAmount'] && !isNaN(Number(json['senderAmount']))) {
+        return 'senderAmount must be a valid ERC20 address';
+      } else if (json['v'] && !isNaN(json['v'])) {
+        ('v must be a number. Make sure it\'s wrapped in quotation marks, e.g. "29"');
+      } else if (json['r'] && typeof json['r'] === 'string') {
+        ('v must be a string. Make sure it\'s wrapped in quotation marks, e.g. "0x67e0723b0afd357d4f28523bf633dfee16e0eab2f3cbcf8ce1afd32a035d2764"');
+      } else if (json['s'] && typeof json['s'] === 'string') {
+        ('s must be a string. Make sure it\'s wrapped in quotation marks, e.g. "0x67e0723b0afd357d4f28523bf633dfee16e0eab2f3cbcf8ce1afd32a035d2764"');
+      }
+    }
     // Check for missing keys
     const missingKeys = requiredKeys.filter((key) => json && !json[key]);
 
     if (missingKeys.length === 0) {
-      return true; // No errors
+      return false; // No errors
     }
 
-    console.log(missingKeys, 'missingKeys');
+    // console.log(missingKeys, 'missingKeys');
 
     const errorString = `Your JSON is missing the following keys: ${missingKeys.join(
       ', '
@@ -34,6 +61,6 @@ export const validateJsonShape = (
     return errorString;
   } catch (e) {
     console.error(e);
-    return 'An unexpected error occurred during validation.';
+    return 'An unexpected error occurred in the validateJsonShape function.';
   }
 };
