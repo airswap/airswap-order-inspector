94d3f43d2c3561672290142a86211342dcd3b6b7
Mike Starr <imhs@protonmail.com>
2024-03-30 13:36:07 -0500

    pasted in remainder of greypixel code. *FULL CREDIT TO GREYPIXEL FOR CODE UP UNTIL THIS COMMIT

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './globals.css';
+import '../globals.css';
 import { ThemeProvider } from './features/theme/ThemeProvider.tsx';
 import { Web3ModalProvider } from './features/wallet/Web3ModalProvider.tsx';
 
