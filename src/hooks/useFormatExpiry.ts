31a1b3425e9773904e429cbd61297181ea8abbc0
Mike Starr <imhs@protonmail.com>
2024-05-06 11:24:35 -0500

    fix useFormatExpiry hook to use formatDistanceToNow utility from date-fns

--- src/hooks/useFormatExpiry.ts
+++ src/hooks/useFormatExpiry.ts
@@ -1,12 +1,14 @@
-import { format } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns';
 
-export const useFormatExpiry = (expiry: number | undefined) => {
+export const useFormatExpiry = (
+  expiry: number | undefined
+): string | undefined => {
   if (!expiry) {
     return undefined;
-  } else {
-    const expiryDate = new Date(expiry * 1000);
-    const formattedDate = format(expiryDate, 'dd/MM/yyyy');
-
-    return formattedDate;
   }
+
+  const expiryDate = new Date(expiry * 1000);
+  const distance = formatDistanceToNow(expiryDate, { addSuffix: true });
+
+  return distance;
 };
